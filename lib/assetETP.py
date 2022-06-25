c7f408fc2038e5cba5c0569d5f33ac0d4378b4f5
Nuwan Waidyanatha <waidyanatha@yahoo.com>
2022-06-25 21:43:03 +0400

    added weighted sum, rolling mean and stdv functions to assetETP class

--- lib/assetETP.py
+++ lib/assetETP.py
@@ -7,6 +7,8 @@
 '''
 class ExchangeTradeProtocol():
 
+    import pandas as pd
+
     ''' Function
             name: __init__
             parameters:
@@ -26,7 +28,75 @@ class ExchangeTradeProtocol():
         return None
 
     ''' Function
-            name: value_index
+            name: weights_matrix
+            parameters:
+                    @name (str)
+                    @clean (dict)
+            procedure: 
+            return DataFrame
+    '''
+    def transpose_pivot(self, data_df):
+
+        import traceback
+        import pandas as pd
+        
+        transp_df = pd.DataFrame([], columns=['Date','ID','market_cap'])
+        _l_dates = data_df['Date'].unique()
+        _l_coin_ids = [col for col in data_df.columns if col !='Date']
+        try:
+            for date in _l_dates:
+                for coin_id in _l_coin_ids:
+                    value = data_df[data_df['Date'] == date][coin_id]
+                    transp_df = pd.concat([transp_df,
+                                           pd.DataFrame({'Date':date, 'ID':coin_id, 'market_cap':value})])
+            transp_df['Date'] = transp_df['Date'].astype('datetime64[ns]')
+            transp_df['market_cap'] = transp_df['market_cap'].astype('float64')
+
+        except Exception as err:
+            _s_fn_id = "Class <ExchangeTradeProtocol> Function <weights_matrix>"
+            print("[Error]"+_s_fn_id, err)
+            print(traceback.format_exc())
+
+        return transp_df
+
+    ''' Function
+            name: get_expected_returns
+            parameters:
+                    @name (str)
+                    @clean (dict)
+            procedure: 
+            return DataFrame
+    '''
+    def sum_weighted_returns(self, data_df : pd.DataFrame, weights):
+
+        import traceback
+        import pandas as pd
+        import numpy as np
+
+#        expected_returns_df = pd.DataFrame()
+        _l_exp_ret = []
+
+        try:
+            if not (data_df.shape[0] > 0):
+                raise ValueError("Invalid dataframe")
+            _l_dates = list(data_df['Date'].unique())
+
+            for date in _l_dates:
+                _top_assets_byDate_df = data_df.loc[data_df['Date'] == date]
+                _top_asset_arr = np.array(_top_assets_byDate_df['market_cap'])
+                weighted_return_arr = np.multiply(_top_asset_arr,weights)
+                sum_weighted_returns = np.sum(weighted_return_arr, axis=1)
+                _l_exp_ret.append({'Date' : date, 'Expected Return' : sum_weighted_returns})  
+
+        except Exception as err:
+            _s_fn_id = "Class <ExchangeTradeProtocol> Function <get_expected_returns>"
+            print("[Error]"+_s_fn_id, err)
+            print(traceback.format_exc())
+
+        return _l_exp_ret
+
+    ''' Function
+            name: get_simple_returns
             parameters:
                     @name (str)
                     @clean (dict)
@@ -57,7 +127,7 @@ class ExchangeTradeProtocol():
         try:
             _l_dates = data_df['Date'].unique()
             _l_topNassets = []
-            topNAssets = pd.DataFrame([],columns=['Date','ID','Value'])
+            topNAssets = pd.DataFrame([],columns=['Date','ID','market_cap'])
             for date in _l_dates:
                 ''' get assets and sort by market cap '''
                 assets = data_df.loc[data_df['Date'] == date]
@@ -65,7 +135,7 @@ class ExchangeTradeProtocol():
                 _l_assetsID = []
                 _l_marketCap = []
                 for row in assets.head(N).iterrows():
-                    asset_dict = {'Date' : date, 'ID' : row[1][1], 'Value' : row[1][3]}
+                    asset_dict = {'Date' : date, 'ID' : row[1][1], 'market_cap' : row[1][3]}
                     topNAssets = pd.concat([topNAssets,pd.DataFrame([asset_dict])])
 
         except Exception as err:
@@ -100,55 +170,58 @@ class ExchangeTradeProtocol():
         return np.around(rand_arr,4)
 
     ''' Function
-            name: rolling_corr
+            name: rolling_mean
             parameters:
                     @name (str)
                     @clean (dict)
             procedure: 
             return DataFrame
     '''
-    def rolling_corr(self, data_df, **params):
+    def rolling_mean(self, data_df, period=7):
 
         import traceback
         import pandas as pd
-        import datetime
-        from datetime import timedelta, date
 
-        corr_matrix = data_df.copy()
+        _rolling_mean = pd.DataFrame()
 
         try:
-            ''' validate dataframe '''
-            if not isinstance(data_df,pd.DataFrame) or not data_df.shape[0] > 0:
-                raise ValueError("Invalid pandas DataFrame")
-            ''' set the number of days to offset from the begining of the time line'''
-            if "days_offset" in params.keys():
-                self.days_offset = params["days_offset"]
-            ''' set the window length in number of days'''
-            if "window_length" in params.keys():
-                self.window_length = params["window_length"]
-            ''' set the whether or not to return statistically significant values '''
-            if "p_val_cutoff" in params.keys():
-                self.p_val = params["p_val_cutoff"]
-
-            for row_idx in range(0,data_df.shape[0]-self.window_length):
-                ''' get the date value from the current datframe'''
-                rolling_day = data_df.iloc[row_idx]["Date"]
-
-                ''' Set the left and right side date of the sliding window '''
-                rolling_win_left_dt = rolling_day + timedelta(days=self.days_offset)
-                rolling_win_right_dt = rolling_win_left_dt + timedelta(days=self.window_length-1)
-
-                mask = (data_df['Date'] >= rolling_win_left_dt) & (data_df['Date'] <= rolling_win_right_dt)
-                corr_df = data_df[mask]
-                print(corr_df.corr())
-                break
+            _l_coin_ids = [col for col in data_df if col !='Date']
+            _rolling_mean = data_df.copy()
+            _rolling_mean[_l_coin_ids] = _rolling_mean[_l_coin_ids].rolling(period).mean()
 
         except Exception as err:
-            _s_fn_id = "Class <ExchangeTradeProtocol> Function <rolling_corr>"
+            _s_fn_id = "Class <ExchangeTradeProtocol> Function <rolling_mean>"
             print("[Error]"+_s_fn_id, err)
             print(traceback.format_exc())
 
-        return corr_matrix
+        return _rolling_mean
+
+    ''' Function
+            name: rolling_stdv
+            parameters:
+                    @name (str)
+                    @clean (dict)
+            procedure: 
+            return DataFrame
+    '''
+    def rolling_stdv(self, data_df, period=7):
+
+        import traceback
+        import pandas as pd
+
+        _rolling_stdv = pd.DataFrame()
+
+        try:
+            _l_coin_ids = [col for col in data_df if col !='Date']
+            _rolling_stdv = data_df.copy()
+            _rolling_stdv[_l_coin_ids] = _rolling_stdv[_l_coin_ids].rolling(period).std()
+
+        except Exception as err:
+            _s_fn_id = "Class <ExchangeTradeProtocol> Function <rolling_stdv>"
+            print("[Error]"+_s_fn_id, err)
+            print(traceback.format_exc())
+
+        return _rolling_stdv
 
     ''' Function
             name: rolling_corr
