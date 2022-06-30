5219967247b81e3ce03ddd2710011623ea894853
Nuwan Waidyanatha <waidyanatha@yahoo.com>
2022-06-30 12:11:47 +0400

    logarithimic returns and changes to class naming
    Signed-off-by: Nuwan Waidyanatha <waidyanatha@yahoo.com>

--- lib/dataETL.py
+++ lib/dataETL.py
@@ -70,6 +70,120 @@ class ExtractLoadTransform():
         return data_df
 
     ''' Function
+            name: get_weighted_index
+            parameters:
+                    @name (str)
+                    @clean (dict)
+            procedure: 
+            return DataFrame
+    '''
+    def get_topN_assets(self,data_df, N = 3):
+
+        import traceback
+        import pandas as pd
+
+        try:
+            _l_dates = data_df['Date'].unique()
+            _l_topNassets = []
+            topNAssets = pd.DataFrame([],columns=['Date','ID','market_cap'])
+            for date in _l_dates:
+                ''' get assets and sort by market cap '''
+                assets = data_df.loc[data_df['Date'] == date]
+                assets = assets.sort_values(by='market_cap',axis=0, ascending = False)
+                _l_assetsID = []
+                _l_marketCap = []
+                for row in assets.head(N).iterrows():
+                    asset_dict = {'Date' : date, 'ID' : row[1][1], 'market_cap' : row[1][3]}
+                    topNAssets = pd.concat([topNAssets,pd.DataFrame([asset_dict])])
+
+        except Exception as err:
+            _s_fn_id = "Class <ExtractLoadTransform> Function <get_topN_assets>"
+            print("[Error]"+_s_fn_id, err)
+            print(traceback.format_exc())
+
+        return topNAssets
+
+    ''' Function
+            name: weights_matrix
+            parameters:
+                    @name (str)
+                    @clean (dict)
+            procedure: 
+            return DataFrame
+    '''
+    def weights_matrix(self, N=3,S=10):
+
+        import numpy as np   #numpy.random
+        import traceback
+
+        try:
+            rand_arr = []
+            rand_arr.append(np.random.dirichlet(np.ones(N),size=S))
+
+        except Exception as err:
+            _s_fn_id = "Class <ExchangeTradeProtocol> Function <weights_matrix>"
+            print("[Error]"+_s_fn_id, err)
+            print(traceback.format_exc())
+
+        return np.around(rand_arr,4)
+
+    ''' Function
+            name: rolling_mean
+            parameters:
+                    @name (str)
+                    @clean (dict)
+            procedure: 
+            return DataFrame
+    '''
+    def rolling_mean(self, data_df, period=7):
+
+        import traceback
+        import pandas as pd
+
+        _rolling_mean = pd.DataFrame()
+
+        try:
+            _l_coin_ids = [col for col in data_df if col !='Date']
+            _rolling_mean = data_df.copy()
+            _rolling_mean[_l_coin_ids] = _rolling_mean[_l_coin_ids].rolling(period).mean()
+            _rolling_mean['Date'] = _rolling_mean['Date'].astype('datetime64[ns]')
+
+        except Exception as err:
+            _s_fn_id = "Class <ExchangeTradeProtocol> Function <rolling_mean>"
+            print("[Error]"+_s_fn_id, err)
+            print(traceback.format_exc())
+
+        return _rolling_mean
+
+    ''' Function
+            name: rolling_stdv
+            parameters:
+                    @name (str)
+                    @clean (dict)
+            procedure: 
+            return DataFrame
+    '''
+    def rolling_stdv(self, data_df, period=7):
+
+        import traceback
+        import pandas as pd
+
+        _rolling_stdv = pd.DataFrame()
+
+        try:
+            _l_coin_ids = [col for col in data_df if col !='Date']
+            _rolling_stdv = data_df.copy()
+            _rolling_stdv[_l_coin_ids] = _rolling_stdv[_l_coin_ids].rolling(period).std()
+            _rolling_stdv['Date'] = _rolling_stdv['Date'].astype('datetime64[ns]')
+
+        except Exception as err:
+            _s_fn_id = "Class <ExchangeTradeProtocol> Function <rolling_stdv>"
+            print("[Error]"+_s_fn_id, err)
+            print(traceback.format_exc())
+
+        return _rolling_stdv
+
+    ''' Function
             name: transfrom_data
             parameters:
                     @name (str)
@@ -77,7 +191,7 @@ class ExtractLoadTransform():
             procedure: 
             return DataFrame
     '''
-    def transfrom_data(self,data_df):
+    def transfrom_data(self,data_df, value_col_name = "Value"):
 
         import pandas as pd
 
@@ -97,12 +211,12 @@ class ExtractLoadTransform():
             tmp_df = tmp_df.dropna(inplace=False)
 
             for _date in tmp_df["Date"]:
-                _value = tmp_df.loc[tmp_df['Date'] == _date, "market_cap"].item()
+                _value = tmp_df.loc[tmp_df['Date'] == _date, value_col_name].item()
                 market_df.loc[market_df['Date']==_date, _s_coin_id] = _value
 
-        ''' Cleanup rows and columns '''
-        market_df=market_df.drop_duplicates()
-        market_df = market_df.dropna(axis=1)
+        ''' DEPRECATED Cleanup rows and columns elsewhere '''
+#        market_df=market_df.drop_duplicates()
+#        market_df = market_df.dropna(axis=1)
 
         ''' Set the dtypes '''
         market_df['Date'] = market_df['Date'].astype('datetime64[ns]')
@@ -115,7 +229,7 @@ class ExtractLoadTransform():
             parameters:
                     @name (str)
                     @clean (dict)
-            procedure: 
+            procedure:
             return DataFrame
     '''
     def transpose_pivot(self, data_df):
@@ -154,16 +268,19 @@ class ExtractLoadTransform():
 
         import traceback
         import pandas as pd
+        import datetime
 
         matching_df = pd.DataFrame([],columns=['Date','ID','Value'])
 
         try:
+            ''' get unique dates from base frame '''
             _l_base_dates = to_base_data_df['Date'].unique()
             for date in _l_base_dates:
+                date = pd.to_datetime(date)
+                ''' get unique coin ids for the date '''
                 coin_ids = to_base_data_df[to_base_data_df['Date']==date]['ID']
                 for c_id in coin_ids:
-                    mask = source_data_df['Date']==date & source_data_df['ID']==c_id
-                    print(mask)
+                    mask = (source_data_df['Date']==date) & (source_data_df['ID']==c_id)
                     value = source_data_df[mask]['market_cap']
                     matching_df = pd.concat([matching_df,
                                              pd.DataFrame({'Date':date,'ID':c_id,'Value':value})])
